3e36dd30d154abb986f8acf4623b8c502e5b3653
Ventsislav Kostadinov <v.kostadinov@avanade.com>
2022-12-01 19:54:09 +0000

    implement navigation component, create global Interface for content, create global css-in-js file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import {useState, useEffect} from "react";
 import './App.css';
+import {Container} from "react-bootstrap"
 import Navigation from "./components/Navigation";
 
 function App() {
+const [value, setValue] = useState<String>("");
+const [clicked, setClicked] = useState<Boolean>(false);
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  
+  }
+ const handleClick = () => {
+  setClicked(!false);
+  console.log(clicked)
+ } 
+
   return (
     <div className="App">
-     <Navigation />
+      <Container>
+      <Navigation handleChange={handleChange} value={value} handleClick={handleClick} />
+      </Container>
+  
     </div>
   );
 }
