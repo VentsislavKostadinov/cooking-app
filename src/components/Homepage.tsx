ea39333cf58bf5df818b735609bbf3e7ae5ba173
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2022-12-03 18:26:45 +0000

    search-logic

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -2,25 +2,40 @@ import React from "react";
 import { useState } from "react";
 import Navigation from "./Navigation";
 import FooterComponent from "./FooterComponent";
-import {Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import api from "../assets/cooking-api.json";
 import { useStyles } from "../assets/useStyle";
 import hyperlinkIcon from "../assets/images/hyperlink-icon.png";
 import PopUp from "./PopUp";
 
 const Homepage = () => {
+  const recipeIngredients = api?.recipes;
+  const ingredients = api?.ingredients;
   const classes = useStyles();
   const [value, setValue] = useState<string>("");
-  const handleChange = (e: any) => {
-    setValue(e.target.value);
-  };
-
   const [recipeDescription, setRecipeDescription] = useState<string[]>([]);
+  const [filteredData, setFilteredData] = useState<any[]>(recipeIngredients);
 
   /* Modal */
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleChange = (e: any) => {
+    const value = e.target.value;
+    setValue(value);
+
+    let ingredientsSearch = [];
+    ingredientsSearch = ingredients.filter((data) => {
+      return data.name.toLowerCase().search(value.toLowerCase()) != -1;
+    });
+
+    let recipeIngredientsSearch = [];
+    recipeIngredientsSearch = recipeIngredients.filter((data) => {
+      return data.title.toLowerCase().search(value.toLowerCase()) != -1;
+    });
+    setFilteredData(recipeIngredientsSearch);
+  };
 
   return (
     <>
@@ -29,7 +44,7 @@ const Homepage = () => {
       </Container>
       <Container className="mt-5 text-center">
         <Row className="gx-md-3 gx-4">
-          {api?.recipes?.map((recipe) => {
+          {filteredData?.map((recipe) => {
             return (
               <Col
                 md
@@ -38,9 +53,9 @@ const Homepage = () => {
               >
                 <h4>{recipe.title}</h4>
                 <p className="text-start">{recipe.timeToPrepare}</p>
-                <img src={recipe.image} alt="recipe image" />
+                <img src={recipe.image} alt="recipe-image" />
                 <ul className="mt-3">
-                  {recipe?.ingredients?.map((recipeElement, index) => {
+                  {recipe?.ingredients?.map((recipeElement: string, index: number) => {
                     return (
                       <li
                         key={index}
@@ -76,6 +91,8 @@ const Homepage = () => {
               </Col>
             );
           })}
+
+          {filteredData.length === 0 && <h3 className="text-center">No result data</h3>}
         </Row>
       </Container>
       <FooterComponent />
